e5cff37c9ecde073f90b087f0599fcb522456af1
Imagiro <thomas.brun.perso@gmail.com>
2024-08-16 01:45:44 +0200

    login remplace home

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Home() {
+function Login() {
   const [username, setUsername] = useState('');
   const navigate = useNavigate();
 
@@ -29,4 +29,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default Login;
